f0c977e1cdd0d07b4220d0aaf4fa1e1e53c7f890
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-23 15:19:25 -0500

    post logic test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bluebird = require("bluebird");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const GameCollection = require("./models/gameCollection");
+const Game = require("./models/gameCollection");
 
 const app = express();
 mongoose.Promise = bluebird;
@@ -12,5 +12,17 @@ mongoose.connect("mongodb://localhost:27017/gameDirectory");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger("dev"));
+
+app.post("/rarebook", (req, res) => {
+  let newGame = new Game(req.body);
+  newGame
+    .save()
+    .then(savedGame => {
+      res.send(savedGame);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
 app.listen(8000, () => console.log("server is running on port 8000"));
